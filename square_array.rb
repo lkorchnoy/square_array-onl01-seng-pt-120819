de24f9831c943d433374a31fe7c972edfaf07e65
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-17 02:08:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,23 +1,3 @@
-def square_array(array)
-  new_array = []
-  array.each { |i| new_array << i ** 2 }
-  new_array
-end
-
-my_array = [1, 2, 3]
-p square_array(my_array) 
-
-
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
-end
-
-my_arr = [1, 2]
-p square_array(my_arr) #=> [1, 4]
-
-
 def square_array(numbers)
   new_numbers = []
   numbers.each { |i| new_numbers << i ** 2 }
@@ -27,11 +7,3 @@ numbers = [1, 2, 3]
 p square_array(numbers)
 
 
-def square_array(numbers)
-  numbers = [1, 2, 3]
-  new_numbers = []
-  numbers.each { |i| new_numbers << i ** 2 }
-  new_numbers
-end
-
-p square_array(numbers)
