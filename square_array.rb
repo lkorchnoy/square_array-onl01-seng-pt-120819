840f712eeb277bb516a7fbe02cccdcbae25fb944
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-17 01:57:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -18,3 +18,11 @@ my_arr = [1, 2]
 p square_array(my_arr) #=> [1, 4]
 
 
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
+end
+
+array = [1, 2]
+p square_array(array) 
